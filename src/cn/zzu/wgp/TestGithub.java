5421e83b57b7b789f8be348cc2461c3d5501332c
wanggongpeng <gongpwang@gmail.com>
2016-11-07 10:56:00 +0800

    test

---
+++ src/cn/zzu/wgp/TestGithub.java
@@ -0,0 +1,11 @@
+package cn.zzu.wgp;
+
+/**
+ * Created by Administrator on 2016/11/07.
+ */
+public class TestGithub {
+
+   public static void main(String[] args) {
+      System.out.println("Hello github");
+   }
+}
